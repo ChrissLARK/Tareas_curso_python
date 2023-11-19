a3559d2483bfb28c9e23515e5f85ec5ffc2fcd16
ChrissLARK <150580318+ChrissLARK@users.noreply.github.com>
2023-11-18 20:39:44 -0500

    Update ejercicio4.py

--- Ejercicios/ejercicio4.py
+++ Ejercicios/ejercicio4.py
@@ -8,13 +8,13 @@ puede ser calculada de la siguiente forma:
 
 numero = int(input("Ingresa un número entero positivo: "))
 
-# Verificar si el número es positivo
+# Verificar si el número es positivo, ya que 0 no es positivo ni negativo, numero > 0 
 if numero <= 0:
     print("Por favor, ingresa un número entero positivo.")
 else:
     suma = 0
 
-    # Calcular la suma de los enteros desde 1 hasta N
+    # Sumanos de los enteros desde 1 hasta el numero que ingrese
     for i in range(1,  numero+ 1):
         suma += i
 
